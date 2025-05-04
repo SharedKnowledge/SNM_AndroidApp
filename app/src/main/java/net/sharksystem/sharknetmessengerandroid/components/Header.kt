3916fa4924066db7ac2de9d12086215f6849c8a2
skaldor <dummy@skaldor.uk>
2025-05-04 20:47:45 +0200

    removed unused classes

--- app/src/main/java/net/sharksystem/sharknetmessengerandroid/components/Header.kt
+++
@@ -1,4 +0,0 @@
-package net.sharksystem.sharknetmessengerandroid.components
-
-class Header {
-}
--- app/src/main/java/net/sharksystem/sharknetmessengerandroid/ui/InitActivity.kt
+++ app/src/main/java/net/sharksystem/sharknetmessengerandroid/ui/InitActivity.kt
@@ -1,10 +1,11 @@
-package net.sharksystem.sharknetmessengerandroid
+package net.sharksystem.sharknetmessengerandroid.ui
 
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import net.sharksystem.sharknetmessengerandroid.R
 import net.sharksystem.sharknetmessengerandroid.sharknet.SharkNetApp
 
 class InitActivity : ComponentActivity(){
@@ -16,9 +17,9 @@ class InitActivity : ComponentActivity(){
         val button = findViewById<Button>(R.id.buttonSubmit)
 
         button.setOnClickListener {
-            sharkNetApp = SharkNetApp(this,peerName.text.toString())
-            SharkNetApp.initialize(this,peerName.text.toString())
-            val testString = SharkNetApp.singleton
+            sharkNetApp = SharkNetApp(this, peerName.text.toString())
+            SharkNetApp.Companion.initialize(this,peerName.text.toString())
+            val testString = SharkNetApp.Companion.singleton
             Toast.makeText(this, "Dein Result: ${testString?.getPeer()?.sharkPeerName} ", Toast.LENGTH_SHORT).show()
         }
     }
